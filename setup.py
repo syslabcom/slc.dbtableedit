bb733ac93e4bea234c79f155d578998461ee4385
pilz <pilz@51ee9ba4-e84d-4896-84de-354f1821c169>
2009-06-05 15:09:41 +0000

    prepared release 1.1

--- setup.py
+++ setup.py
@@ -1,33 +1,69 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+"""
+This module contains the slc.dbtableedit package
+"""
 import os
+from setuptools import setup, find_packages
 
-version = '0.1'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.1'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('slc', 'dbtableedit', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' 
+    )
+    
+tests_require=['zope.testing']
 
 setup(name='slc.dbtableedit',
       version=version,
       description="Edit and View simple RDB Tables",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='edit relational database table',
-      author='Syslab.comGmbH',
+      author='Syslab.com GmbH',
       author_email='info@syslab.com',
-      url='http://svn.plone.org/svn/collective/slc.dbtableedit',
+      url='https://svn.syslab.com/svn/syslabcom/slc.dbtableedit/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['slc'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
+          'setuptools',    
           'collective.lead>=1.0b3,<2.0dev'
+          # -*- Extra requirements: -*-
       ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'slc.dbtableedit.tests.test_docs.test_suite',
       entry_points="""
-      # -*- Entry points: -*-
+      # -*- entry_points -*- 
       """,
-      )
+      paster_plugins = ["ZopeSkel"],
+      )      
+      
